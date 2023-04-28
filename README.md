c2736fe4c98bf0957c713b332736c43c9b9c6508
Bexultan <mustafin.beksultan04@gmail.com>
2023-04-28 22:07:06 +0600

    maze

---
+++ README.md
@@ -0,0 +1,18 @@
+
+## Requirement :
+
+To launch the game you need to have python installed and to install the needed packages with pygame
+---
+
+## Tools used:
+* Pygame: 
+  * https://www.pygame.org
+
+---
+## How to play?
+* Press the w, a, s, d buttons
+---
+
+## Credits (royalty free for non-commercial_only):
+* Music: 
+  * https://soundcloud.com/adhesivewombat
---
+++ images.py
@@ -0,0 +1,20 @@
+import pygame
+import os
+from settings import IMAGES_ROOT
+
+try:
+    BUTTONS_IMAGES = {
+        "blue-button": pygame.image.load(os.path.join(IMAGES_ROOT, "button1-blue.png")),
+        "red-button": pygame.image.load(os.path.join(IMAGES_ROOT, "button1-red.png"))
+    }
+    BG_IMAGE = pygame.image.load(os.path.join(IMAGES_ROOT, "main-background.png"))
+    LEVEL_IMG = pygame.image.load(os.path.join(IMAGES_ROOT, "level-bg.png"))
+    BLOCK_IMAGE = pygame.image.load(os.path.join(IMAGES_ROOT, "block.png"))
+    TREASURE_IMAGE = pygame.image.load(os.path.join(IMAGES_ROOT, "trophy.png"))
+    PLAYER_IMAGE = pygame.image.load(os.path.join(IMAGES_ROOT, "player.png"))
+    TEXT_BG_IMAGE = pygame.image.load(os.path.join(IMAGES_ROOT, "text_bg.png"))
+    icon = pygame.display.set_icon(pygame.image.load("img\\maze_icon.png"))
+    
+except pygame.error:
+    print("Missing images")
+    exit()
